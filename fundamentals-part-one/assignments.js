6e8242612d1b81d4304223e668b3f073e4c5db16
Brendan Madden <brendanmaddento@gmail.com>
2021-08-18 00:47:54 -0400

    initial commit for javascript course

---
+++ fundamentals-part-one/assignments.js
@@ -0,0 +1,23 @@
+/*
+// Notes
+
+- It's the value, not the variable, that determines the data type.
+- When you declare a variable but do not set a value, the variable is undefined, and the value is undefined. Ex:
+let year;
+year is an undefined variable type, and it's value is undefined.
+- There is a legacy bug in the browser that defines typeof null as an object, but this is not correct.
+
+// Assignments
+
+let country = "Canada";
+let continent = "North America";
+let population = 37500000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let x = true;
+
+console.log(typeof x);
+*/
