45efc4a43a30ee6eb24c1a6433e05d187fc87246
Brendan Madden <brendanmaddento@gmail.com>
2021-09-14 15:28:26 -0400

    fixing syntax error

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -68,16 +68,16 @@ document.querySelector(".check").addEventListener("click", function () {
   //   }
   // });
 
-//   document.querySelector(".again").addEventListener("click", function () {
-//     score = 20;
-//     secretNumber = Math.trunc(Math.random() * 20) + 1;
+  //   document.querySelector(".again").addEventListener("click", function () {
+  //     score = 20;
+  //     secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-//     document.querySelector(".message").textContent = "Start guessing...";
-//     document.querySelector(".guess").value = "";
-//     document.querySelector(".number").textContent = "?";
-//     document.querySelector(".score").textContent = score;
+  //     document.querySelector(".message").textContent = "Start guessing...";
+  //     document.querySelector(".guess").value = "";
+  //     document.querySelector(".number").textContent = "?";
+  //     document.querySelector(".score").textContent = score;
 
-//     document.querySelector("body").style.backgroundColor = "#222";
-//     document.querySelector(".number").style.width = "15rem";
-//   });
-// });
+  //     document.querySelector("body").style.backgroundColor = "#222";
+  //     document.querySelector(".number").style.width = "15rem";
+  //   });
+});
