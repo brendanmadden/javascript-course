02dfc8c6edb7f9c60ca9c2a3af1075fa390d4068
Brendan Madden <brendanmaddento@gmail.com>
2021-09-14 16:11:37 -0400

    cleaned up script

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -40,44 +40,43 @@ document.querySelector(".check").addEventListener("click", function () {
     }
   }
 
-  //   Guess is too high
-  //   } else if (guess > secretNumber) {
-  //     if (score > 1) {
-  //       document.querySelector(".message").textContent =
-  //         "⬆️ Your guess is too high!";
-  //       score--;
-  //       document.querySelector(".score").textContent = score;
-  //     } else {
-  //       document.querySelector(".message").textContent =
-  //         "😭 You ran out of points!";
-  //       document.querySelector(".score").textContent = 0;
-  //     }
+  // Guess is too high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent =
+        "⬆️ Your guess is too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "😭 You ran out of points!";
+      document.querySelector(".score").textContent = 0;
+    }
 
-  //     //   Guess is too low
-  //   } else if (guess < secretNumber) {
-  //     if (score > 1) {
-  //       document.querySelector(".message").textContent =
-  //         "⬇️ Your guess is too low!";
-  //       score--;
-  //       document.querySelector(".score").textContent = score;
-  //     } else {
-  //       document.querySelector(".message").textContent =
-  //         "😭 You ran out of points!";
-  //       document.querySelector(".score").textContent = 0;
-  //     }
-  //   }
-  // });
+    //   Guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent =
+        "⬇️ Your guess is too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "😭 You ran out of points!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
 
-  //   document.querySelector(".again").addEventListener("click", function () {
-  //     score = 20;
-  //     secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  //     document.querySelector(".message").textContent = "Start guessing...";
-  //     document.querySelector(".guess").value = "";
-  //     document.querySelector(".number").textContent = "?";
-  //     document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = score;
 
-  //     document.querySelector("body").style.backgroundColor = "#222";
-  //     document.querySelector(".number").style.width = "15rem";
-  //   });
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
