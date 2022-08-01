8c627475e690cb41171aceca259b30307f71804e
Brendan Madden <brendanmaddento@gmail.com>
2022-07-31 21:58:21 -0400

    working on javascript course

--- fundamentals-part-one/starter/assignment.js
+++ fundamentals-part-one/starter/assignment.js
@@ -1,5 +1,7 @@
-let country = "Canada";
-let continent = "North America";
+/*
+
+const country = "Canada";
+const continent = "North America";
 let population = 37500000;
 
 // alert("continent");
@@ -7,3 +9,24 @@ let population = 37500000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+const isIsland = false;
+const language = "English";
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+*/
+
+let markMass = 78;
+let markHeight = 1.69;
+let markBMI = markMass / markHeight ** 2;
+let johnMass = 92;
+let johnHeight = 1.95;
+let johnBMI = johnMass / johnHeight ** 2;
+
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI, markBMI, johnBMI);
