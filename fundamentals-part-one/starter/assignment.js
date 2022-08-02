776dbf2c63c8243cd6baea709ddca730e09029e8
Brendan Madden <brendanmaddento@gmail.com>
2022-08-01 21:36:38 -0400

    working on javascript course

--- fundamentals-part-one/starter/assignment.js
+++ fundamentals-part-one/starter/assignment.js
@@ -18,7 +18,7 @@ console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
 
-*/
+
 
 let markMass = 78;
 let markHeight = 1.69;
@@ -37,4 +37,25 @@ if (markBMI > johnBMI) {
   );
 } else {
   console.log(`John's BMI of ${johnBMI} is higher than Mark's`);
+}
+
+*/
+
+// Coding Challenge #3
+
+let avgDolphins = (96 + 108 + 89) / 3;
+let avgKoalas = (88 + 91 + 110) / 3;
+
+if (avgDolphins > avgKoalas && avgDolphins >= 100) {
+  console.log(`Dolphins Win!`);
+} else if (avgDolphins < avgKoalas && avgKoalas >= 100) {
+  console.log(`Koalas Win!`);
+} else if (
+  avgDolphins === avgKoalas &&
+  avgDolphins >= 100 &&
+  avgKoalas >= 100
+) {
+  console.log(`It's a tie!`);
+} else {
+  console.log(`Nobody wins because the avg score is below 100`);
 }
