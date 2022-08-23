a1939b127da41b8831c9c34e45fddd957487363a
Brendan Madden <brendanmaddento@gmail.com>
2022-08-23 14:11:42 -0400

    working on js course

--- data-structures-operators/script.js
+++ data-structures-operators/script.js
@@ -16,6 +16,10 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(starterIndex, mainIndex, address, time);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -32,6 +36,55 @@ const restaurant = {
   },
 };
 
+const arr = [7, 8, 9];
+const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArray);
+
+const newArray = [1, 2, ...arr];
+console.log(newArray);
+console.log(...newArray); // = writing 1, 2, 7, 8, 9
+
+const newMenu = [...restaurant.mainMenu, `Gnocci`];
+console.log(newMenu);
+
+////////////////////////////////////////
+//DESTRUCTURING OBJECTS
+/*
+
+restaurant.orderDelivery({
+  time: `22:30`,
+  address: `Via Del Sol, 21`,
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+const { name, openingHours, categories } = restaurant; //DESTRUCTURING OBJECT
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags, // giving the properties new variable names
+} = restaurant;
+
+//SET DEFAULT VALUES
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//MUTATING VARIABLES
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+//NESTED OBJECTS
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+*/
+/*
+////////////////////////////////////////
+// DESTRUCTURING ARRAYS
 const arr = [2, 3, 4];
 
 const a = arr[0];
@@ -73,3 +126,5 @@ console.log(i, j, k);
 //DEFAULT VALUES
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
+
+*/
