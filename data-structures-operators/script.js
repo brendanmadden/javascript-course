c30e4f7ffa803f59cc7e0287aae9eeb1d9f9313b
Brendan Madden <brendanmaddento@gmail.com>
2022-08-23 22:14:14 -0400

    working onn js course

--- data-structures-operators/script.js
+++ data-structures-operators/script.js
@@ -84,9 +84,9 @@ game.odds.team1 < game.odds.team2 &&
   console.log(`${game.team2} has the higher odds`);
 
 /////////////////////////////////////////////////////////////////
-*/
 
-//////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
 //CODING CHALLENGE 2
 const game = {
   team1: "Bayern Munich",
@@ -155,6 +155,10 @@ for (const player of game.scored) {
   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
   console.log(scorers);
 }
+*/
+////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////
 // Data needed for a later exercise
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
@@ -209,6 +213,76 @@ const restaurant = {
 };
 
 /*
+//////////////////////////////////////////////////////
+// Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+///////////////////////////////////////////////////
+// Sets
+const ordersSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+console.log(ordersSet);
+
+console.log(new Set("Jonas"));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has("Pizza"));
+console.log(ordersSet.has("Bread"));
+ordersSet.add("Garlic Bread");
+ordersSet.add("Garlic Bread");
+ordersSet.delete("Risotto");
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+  new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
+);
+
+console.log(new Set("jonasschmedtmann").size);
+
+
 ///////////////////////////////////////
 // Looping Objects: Object Keys, Values, and Entries
 
