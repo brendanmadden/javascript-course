89bc14be6c3a66f769d65f86eba7c96d7f9d9bbb
Brendan Madden <brendanmaddento@gmail.com>
2022-08-24 16:25:54 -0400

    working on js course

--- data-structures-operators/script.js
+++ data-structures-operators/script.js
@@ -158,6 +158,44 @@ for (const player of game.scored) {
 */
 ////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////
+//CODING CHALLENGE 3
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+const time = [...gameEvents.keys()].pop();
+console.log(gameEvents.size);
+console.log(
+  `On average, an event happened every ${time / gameEvents.size} minutes`
+);
+
+// 4. [FIRST HALF] 17: ⚽   GOAL
+for (const [i, el] of gameEvents) {
+  const half = i <= 45 ? `FIRST` : `SECOND`;
+  console.log(`[${half} HALF]: Minute ${i}: ${el} occured`);
+}
+
 ////////////////////////////////////////////////////
 // Data needed for a later exercise
 const flights =
