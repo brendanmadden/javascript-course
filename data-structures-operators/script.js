781737b9909c5f382108de8c3eebb6a06a48699a
Brendan Madden <brendanmaddento@gmail.com>
2022-08-26 13:37:59 -0400

    completed section 9 data sctructures and operators

--- data-structures-operators/script.js
+++ data-structures-operators/script.js
@@ -195,7 +195,7 @@ for (const [i, el] of gameEvents) {
   const half = i <= 45 ? `FIRST` : `SECOND`;
   console.log(`[${half} HALF]: Minute ${i}: ${el} occured`);
 }
-*/
+
 
 /////////////////////////////////////////////////////////////////
 //CODING CHALLENGE 4
@@ -245,10 +245,30 @@ document.querySelector(`button`).addEventListener(`click`, function () {
 //   }
 //   console.log(namesUpper.join(" "));
 // };
+
+*/
 ////////////////////////////////////////////////////
 // Data needed for a later exercise
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const flightsArr1 = flights.split("+");
+console.log(flightsArr1);
+
+for (const [i, val] of flightsArr1.entries()) {
+  const [type, from, to, time] = val.split(";");
+  const output = `${type.includes(`Delayed`) ? `🔴` : ``}${type
+    .trim()
+    .replaceAll(`_`, " ")} from ${from.toUpperCase().slice(0, 3)} to ${to
+    .toUpperCase()
+    .slice(0, 3)} (${time.replace(`:`, `h`)})`.padStart(45);
+  console.log(output);
+}
 
 // Data needed for first part of the section
 const days = [`mon`, `tues`, `wed`, `thurs`, `fri`, `sat`, `sun`];
