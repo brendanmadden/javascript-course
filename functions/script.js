44e11760181d12c6852d4a7f5e306c8e71129365
Brendan Madden <brendanmaddento@gmail.com>
2022-08-27 16:38:21 -0400

    working on js course

--- functions/script.js
+++ functions/script.js
@@ -337,11 +337,13 @@ runOnce();
 // console.log(isPrivate);
 console.log(notPrivate);
 
-*/
+
 
 // Closure makes a function remember everything (variables) that existed at it's birthplace (at the time it was created)
 // 👉 A function has access to the variable environment(VE) of the execution context in which it was created
 // 👉 Closure: VE attached to the function, exactly as it was at the time and place the function was created
+
+// A closure has priority over the SCOPE CHAIN
 
 // A closure is the closed-over variable environment of the execution context in which a function was created, even aftert hat execution context is gone;
 
@@ -352,3 +354,82 @@ console.log(notPrivate);
 // A closure is like a backpack that a function carries around wherever it goes. This backpack has all the variables that were present in the environment where the function was created.
 
 // We do NOT have to manually create closures, this is a JavaScript feature that happens automatically. We can’t even access closed-over variables explicitly. A closure is NOT a tangible JavaScript object.
+
+///////////////////////////////////////
+// Closures
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+
+///////////////////////////////////////
+// More Closure Examples
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  document.querySelector(`body`).addEventListener(`click`, function () {
+    header.style.color = `blue`;
+  });
+})();
+
+*/
