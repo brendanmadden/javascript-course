d72b3e74f75251cea9b37916469b1b8dbe99fa23
Brendan Madden <brendanmaddento@gmail.com>
2022-08-29 13:14:34 -0400

    working on js course

--- arrays-bankist/script.js
+++ arrays-bankist/script.js
@@ -315,7 +315,6 @@ const createUsernames = function (accs) {
 createUsernames(accounts);
 console.log(accounts);
 
-*/
 
 ///////////////////////////////////////
 // The filter Method
@@ -349,3 +348,91 @@ const check = movements.reduce(function (acc, mov, i, arr) {
   return acc;
 }, movements[0]);
 console.log(check);
+
+///////////////////////////////////////
+// The filter Method
+const deposits = movements.filter(function (mov, i, arr) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
+
+///////////////////////////////////////
+// The reduce Method
+console.log(movements);
+
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
+
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+// const ages = [5, 2, 4, 1, 15, 8, 3];
+
+// const calcAvgHumanAge = function (ages) {
+//   const humanAges = ages.map((age) =>
+//     age <= 2 ? (age = 2 * age) : (age = 16 + age * 4)
+//   );
+//   console.log(humanAges);
+
+//   const olderDogsHumanAges = humanAges.filter((age) => age > 18);
+//   console.log(olderDogsHumanAges);
+
+//   const avgOlderDogAgeInHuman = olderDogsHumanAges.reduce(
+//     (acc, cur, i, arr) => acc + cur / arr.length,
+//     0
+//   );
+
+//   return avgOlderDogAgeInHuman;
+// };
+
+// const avg1 = calcAvgHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg2 = calcAvgHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1, avg2);
+
+// const calcAverageHumanAge = function (ages) {
+//   const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
+//   const adults = humanAges.filter((age) => age >= 18);
+//   console.log(humanAges);
+//   console.log(adults);
+
+//   // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+//   const average = adults.reduce(
+//     (acc, age, i, arr) => acc + age / arr.length,
+//     0
+//   );
+
+//   // 2 3. (2+3)/2 = 2.5 === 2/2+3/2 = 2.5
+
+//   return average;
+// };
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1, avg2);
