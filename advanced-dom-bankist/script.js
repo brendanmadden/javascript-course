c91e9cb864d4664ebefa63910f2858b942f2d4d6
Brendan Madden <brendanmaddento@gmail.com>
2022-09-05 18:40:50 -0400

    working on js course

--- advanced-dom-bankist/script.js
+++ advanced-dom-bankist/script.js
@@ -130,3 +130,30 @@ logo.classList.contains("c"); // not includes
 logo.className = "jonas";
 
 */
+
+const btnScrollTo = document.querySelector(`.btn--scroll-to`);
+const section1 = document.querySelector(`#section--1`);
+
+btnScrollTo.addEventListener(`click`, function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log(window.scrollX, window.scrollY);
+  console.log(
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+  // Scrolling ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX,
+  //   s1coords.top + window.scrollY
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: `smooth`,
+  // });
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
