08162152287f724428477abeada1ab829c7cab29
Brendan Madden <brendanmaddento@gmail.com>
2022-09-11 17:44:20 -0400

    working on js course

--- advanced-dom-bankist/script.js
+++ advanced-dom-bankist/script.js
@@ -17,6 +17,7 @@ const imgTargets = document.querySelectorAll(`img[data-src]`);
 const slides = document.querySelectorAll(`.slide`);
 const btnLeft = document.querySelector(`.slider__btn--left`);
 const btnRight = document.querySelector(`.slider__btn--right`);
+const dotContainer = document.querySelector(`.dots`);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Modal window ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -223,9 +224,11 @@ imgTargets.forEach((img) => imgObserver.observe(img));
 let curSlide = 0;
 const maxSlide = slides.length;
 
+const createDots = function () {};
+
 const goToSlide = function (slide) {
   slides.forEach(
-    (s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
   );
 };
 goToSlide(0);
@@ -240,7 +243,22 @@ const nextSlide = function () {
   goToSlide(curSlide);
 };
 
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
 btnRight.addEventListener(`click`, nextSlide);
+btnLeft.addEventListener(`click`, prevSlide);
+
+document.addEventListener(`keydown`, function (e) {
+  if (e.key === `ArrowLeft`) prevSlide();
+  e.key === `ArrowRight` && nextSlide(); // Using short circuiting - just another way to do the line above
+});
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
