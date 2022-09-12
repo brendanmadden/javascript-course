6efbb0639d12d6943b1b13a2aa8e9e7cdc2a2143
Brendan Madden <brendanmaddento@gmail.com>
2022-09-11 22:28:20 -0400

    working on js course

--- OOP/script.js
+++ OOP/script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const Person = function (firstName, birthYear) {
+  // Instance Properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // NEVER DO THIS
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const brendan = new Person(`Brendan`, 1991);
+console.log(brendan);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+const john = new Person(`john`, 2017);
+const jill = new Person(`jill`, 1867);
+
+console.log(john, jill);
+console.log(brendan instanceof Person);
