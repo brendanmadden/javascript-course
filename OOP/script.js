8a0dc04149514dd75ba962972d46fa9e610eddad
Brendan Madden <brendanmaddento@gmail.com>
2022-09-25 20:55:17 -0400

    working on js course

--- OOP/script.js
+++ OOP/script.js
@@ -379,22 +379,19 @@ console.log(mike instanceof Object);
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
 
-*/
 
-// 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 'speed' property. The 'speed' property is the current speed of the car in km/h
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Coding Challenge #3 /////////////////////////////////////////////////////////////////////////////////////////////
+
 const Car = function (make, speed) {
   this.speed = speed;
   this.make = make;
 };
 
-// 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console
-
 Car.prototype.accelerate = function () {
   this.speed += 10;
   console.log(`${this.make} is going at ${this.speed}km/h`);
 };
-
-// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console
 
 Car.prototype.brake = function () {
   this.speed -= 5;
@@ -406,6 +403,7 @@ const EV = function (make, speed, charge) {
   this.charge = charge;
 };
 
+// Link the prototypes
 EV.prototype = Object.create(Car.prototype);
 
 EV.prototype.chargeBattery = function (chargeTo) {
@@ -424,10 +422,122 @@ const tesla = new EV(`Tesla`, 160, 50);
 tesla.accelerate();
 tesla.accelerate();
 tesla.chargeBattery(100);
-tesla.accelerate();
+tesla.accelerate(); // EV Tesla using it's own accelerate method is an example of polymorphism because Car also has an accelerate method
 tesla.brake();
 tesla.brake();
 tesla.brake();
 tesla.brake();
 console.log(tesla.__proto__);
 console.log(EV.prototype.constructor);
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Inheritance Between "Classes": ES6 Classes //////////////////////////////////////////////////////////////////////
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log("Hey there 👋");
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Always needs to happen first!
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl("Martha Jones", 2012, "Computer Science");
+martha.introduce();
+martha.calcAge();
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Inheritance Between "Classes": Object.create ////////////////////////////////////////////////////////////////////
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init("Jay", 2010, "Computer Science");
+jay.introduce();
+jay.calcAge();
+
+*/
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+  }
+}
+
+const acc1 = new Account(`Jonas`, `EUR`, 1111);
+console.log(acc1);
