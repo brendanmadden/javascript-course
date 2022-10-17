e1361755c95ed613efca196f0093f2f056b3d810
Brendan Madden <brendanmaddento@gmail.com>
2022-10-17 19:43:56 -0400

    working on js course

--- asynchronous-javascript/script.js
+++ asynchronous-javascript/script.js
@@ -5,4 +5,20 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
-///////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open(`GET`, `https://restcountries.com/v2/name/portugal`);
+request.send();
+
+request.addEventListener(`load`, function () {
+  console.log(this.responseText);
+});
+
+const request2 = new XMLHttpRequest();
+request2.open(`GET`, `https://restcountries.com/v2/name/canada`);
+request2.send();
+
+request2.addEventListener(`load`, function () {
+  console.log(this.responseText);
+});
