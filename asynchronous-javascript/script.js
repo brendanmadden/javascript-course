9a705f1e4b25537235c25729fd159bf57b3a6a75
Brendan Madden <brendanmaddento@gmail.com>
2022-10-17 14:07:35 -0400

    working on js course

--- asynchronous-javascript/script.js
+++ asynchronous-javascript/script.js
@@ -1,6 +1,8 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+// correct API URL: https://restcountries.com/v2/
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
