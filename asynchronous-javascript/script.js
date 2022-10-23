3ca08a1f4a8b05205a659088508689415f42cb86
Brendan Madden <brendanmaddento@gmail.com>
2022-10-23 19:39:58 -0400

    working on js course

--- asynchronous-javascript/script.js
+++ asynchronous-javascript/script.js
@@ -543,3 +543,56 @@ const get3Countries = async function (c1, c2, c3) {
 };
 get3Countries(`portugal`, `canada`, `tanzania`);
 */
+
+// Promise.race
+(async function () {
+  try {
+    const response = await Promise.race([
+      getJSON(`https://restcountries.com/v2/name/italy`),
+      getJSON(`https://restcountries.com/v2/name/egypt`),
+      getJSON(`https://restcountries.com/v2/name/mexico`),
+    ]);
+    console.log(response[0]);
+  } catch (err) {
+    console.error(`${err.message} oops`);
+  }
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long!`));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v2/name/tanzania`),
+  timeout(5),
+])
+  .then((res) => console.log(res[0]))
+  .catch((err) => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve(`Success`),
+  Promise.reject(`ERROR`),
+  Promise.resolve(`Another Success`),
+]).then((res) => console.log(res));
+
+Promise.all([
+  Promise.resolve(`Success`),
+  Promise.reject(`ERROR`),
+  Promise.resolve(`Another Success`),
+])
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+
+// Promise.any [ES2021] (Similar to Promise.race but rejected promises are ignored)
+Promise.any([
+  Promise.resolve(`Success`),
+  Promise.reject(`ERROR`),
+  Promise.resolve(`Another Success`),
+])
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
