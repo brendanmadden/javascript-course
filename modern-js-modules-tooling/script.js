e3301da59d08bda1f28a5ad994e703bdbfea410c
Brendan Madden <brendanmaddento@gmail.com>
2022-10-25 12:39:12 -0400

    working on js course

--- modern-js-modules-tooling/script.js
+++ modern-js-modules-tooling/script.js
@@ -1,2 +1,9 @@
 // Importing Module
+// All modules are executed in strict mode by default
+import { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
+("./shoppingCart.js");
+
 console.log(`Importing Module`);
+
+addToCart(`bread`, 5);
+console.log(price, tq);
